35c5c5c284964ce3c13112feb55ea98093106462
Ivan Rosic <ivarosic@z4r3p3.42lyon.fr>
2021-03-22 17:48:47 +0100

    ADD is finish on ex01 need SEARCH for finish exercice

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: ivarosic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:54:08 by ivarosic          #+#    #+#             */
-/*   Updated: 2021/03/19 13:10:00 by ivarosic         ###   ########lyon.fr   */
+/*   Updated: 2021/03/22 17:47:13 by ivarosic         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,108 @@
 #include "phonebook.hpp"
 
 Phonebook::Phonebook( void ){
-	std::cout << "constructor called phonebook" << std::endl;
+	_i = 0;
 }
 
 Phonebook::~Phonebook( void ){
-	std::cout << "destructor called phonebook" << std::endl;
 }
 
-void p::_ft_add(void)
-{	
+void Phonebook::_ft_add(void)
+{
+	if (_i >= 8)
+		return;
+	std::string line;
 	std::cout << "first_name : ";
-	std::cin >> c[i].first_name;
-/*
+	std::getline(std::cin, line);
+	_c[_i].first_name = line;
+
 	std::cout << "last_name : ";
-	std::cin >> c[i].last_name;
+	std::getline(std::cin, line);
+	_c[_i].last_name = line;
 
 	std::cout << "nickname : ";
-	std::cin >> c[i].nickname;
+	std::getline(std::cin, line);
+	_c[_i].nickname = line;
 
 	std::cout << "login : ";
-	std::cin >> c[i].login;
+	std::getline(std::cin, line);
+	_c[_i].login = line;
 
 	std::cout << "postal address : ";
-	std::cin >> c[i].postal_address;
+	std::getline(std::cin, line);
+	_c[_i].postal_address = line;
 
 	std::cout << "email address : ";
-	std::cin >> c[i].email_address;
+	std::getline(std::cin, line);
+	_c[_i].email_address = line;
 
 	std::cout << "phone number : ";
-	std::cin >> c[i].phone_number;
+	std::getline(std::cin, line);
+	_c[_i].phone_number = line;
 
 	std::cout << "birthday date : ";
-	std::cin >> c[i].birthday_date;
+	std::getline(std::cin, line);
+	_c[_i].birthday_date = line;
 
 	std::cout << "favorite meal : ";
-	std::cin >> c[i].favorite_meal;
+	std::getline(std::cin, line);
+	_c[_i].favorite_meal = line;
 
 	std::cout << "underwear color : ";
-	std::cin >> c[i].underwear_color;
+	std::getline(std::cin, line);
+	_c[_i].underwear_color = line;
 
 	std::cout << "darkest secret : ";
-	std::cin >> c[i].darkest_secret;*/
+	std::getline(std::cin, line);
+	_c[_i].darkest_secret = line;
+
+	_i++;
+}
+
+void Phonebook::_ft_print_space(std::string line)
+{
+
+}
+void Phonebook::_ft_print_all(void)
+{
+	int n = 0;
+	std::string line;
+
+	std::cout << "index     |prenom    |nom       |pseudo    " << std::endl;
+	while(n < _i)
+	{
+		std::cout << n << "         |";
+		line = _c[n].first_name;
+		std::cout << line.size() << std::endl;
+		n++;
+	}
+
+}
+
+void Phonebook::_ft_search(void)
+{
+	if(_i == 0)
+	{
+	std::cout << "need contact for search" << std::endl;
+		return;
+	}
+	_ft_print_all();
+
+}
+
+int Phonebook::ft_cmd(std::string cmd)
+{
+	if(cmd == "EXIT")
+		return 0;
+	else if(cmd == "ADD")
+	{
+		_ft_add();
+	}
+	else if(cmd == "SEARCH")
+		_ft_search();
+	else
+		std::cout << "only ADD, SEARCH or EXIT has accepted" << std::endl;
+	return(1);
 }
 
 int main(void)
@@ -65,18 +126,9 @@ int main(void)
 	while(1)
 	{
 		std::cout << "prompt> ";
-		//use get_in
-		std::cin >> cmd;
-		if(cmd == "EXIT")
-			return 0;
-		else if(cmd == "ADD")
-		{
-				_ft_add();
-		}
-		else if(cmd == "SEARCH")
-			;
-		else
-			std::cout << "only ADD, SEARCH or EXIT has accepted" << std::endl;
+		std::getline(std::cin, cmd);
+		if(p.ft_cmd(cmd) == 0)
+			return(0);
 	}
 	return 0;
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,19 +6,20 @@
 /*   By: ivarosic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:44:41 by ivarosic          #+#    #+#             */
-/*   Updated: 2021/03/19 13:10:03 by ivarosic         ###   ########lyon.fr   */
+/*   Updated: 2021/03/22 17:47:11 by ivarosic         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+
 class	Phonebook	{
 
 	private:
 
 		class	Contact	{
-		
-			private:
+
+			public:
 				std::string first_name;
 				std::string last_name;
 				std::string nickname;
@@ -32,13 +33,17 @@ class	Phonebook	{
 				std::string darkest_secret;
 
 		};
-		Contact c[8];
-		int i;
-
+		Contact _c[8];
+		int _i;
 		void _ft_add(void);
+		void _ft_search(void);
+		void _ft_print_all(void);
+		void _ft_print_all(std::string line);
+
 
 	public:
 
+		int ft_cmd(std::string cmd);
 		Phonebook( void );
 		~Phonebook( void );
 
